d00fed25cd0bcfe6f8f45ce10866aff25e6993ce
JATIN <s.jatin1903@gmail.com>
2020-02-08 11:39:00 -0800

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 // RHL only for front end development
 import { hot } from 'react-hot-loader';
 
+import Login from './login/Login';
+
 class App extends Component {
   render() {
-    return <div>kasdj</div>;
+    return <Login/>;
   }
 }
 // hot export works with RHL. Remove line 11 when starting fullstack integration
